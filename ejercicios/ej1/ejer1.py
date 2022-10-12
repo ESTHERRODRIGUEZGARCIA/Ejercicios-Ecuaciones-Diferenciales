b6ed54e98f39b684c692b5ce14a3945417400722
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-12 18:57:18 +0200

    hemos descubierto américa, PPRINT AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- ejercicios/ej1/ejer1.py
+++ ejercicios/ej1/ejer1.py
@@ -5,14 +5,13 @@ import numpy as np
 import sympy as sp
 import math
 from sympy import init_printing
-#Para un mejor despliegue de la ED
 
 x,y = symbols("x y")
 f,g =map(Function,'fg')
 
 #ED : y'=y*x*sin(x)
-ed = Eq(Derivative(f(x),x),f(x)*x*sin(x))
-
-solucion=dsolve(ed,f(x))
+ed = Eq(f(x)*Derivative(f(x),x),(f(x)+1)**2/(f(x)**2)
+eq = y(x)*ln(x)*y(x).diff(x)-((y(x)+1)/(x))**2
+sol = dsolve(ed,f(x))
 print("El resultado es:")
-pprint(solucion)
+pprint(sol)
