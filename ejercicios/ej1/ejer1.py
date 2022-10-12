6c75d74f46a71e226728b94bea3292257c403e72
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-12 18:50:16 +0200

    bieenmnnnn

--- ejercicios/ej1/ejer1.py
+++ ejercicios/ej1/ejer1.py
@@ -4,3 +4,15 @@ from sympy import *
 import numpy as np
 import sympy as sp
 import math
+from sympy import init_printing
+#Para un mejor despliegue de la ED
+
+x,y = symbols("x y")
+f,g =map(Function,'fg')
+
+#ED : y'=y*x*sin(x)
+ed = Eq(Derivative(f(x),x),f(x)*x*sin(x))
+
+solucion=dsolve(ed,f(x))
+print("El resultado es:")
+pprint(solucion)
