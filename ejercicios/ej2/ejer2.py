83641fe57f680c90dc0db9c2a5591f1471cc29a1
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-12 19:33:20 +0200

    porqué no me va :(?

--- ejercicios/ej2/ejer2.py
+++ ejercicios/ej2/ejer2.py
@@ -1,9 +1,21 @@
-x,y = symbols("x y")
-f,g =map(Function,'fg')
+# importo librerías
+from sympy import *
+import numpy as np
+import sympy 
+from sympy.interactive import printing
+printing.init_printing(use_latex=True)
+import math
+from sympy import init_printing
+import matplotlib.pyplot as plt
 
-#ED : y'=y*x*sin(x)
-ed = Eq(f(x)*Derivative(f(x),x),(f(x)+1)**2/(f(x)**2)
-eq = y(x)*ln(x)*y(x).diff(x)-((y(x)+1)/(x))**2
-sol = dsolve(ed,f(x))
-print("El resultado es:")
-pprint(sol)
+x,y = symbols('x,y')
+f,g = map(Function,'fg')
+
+#Ecuacion 1
+print("Ecuacion 1:")
+ed= Eq(Derivative(f(x),x),(f(x)(x*2)-f(x))/(f(x)+1))
+ci = {f(3): -1}
+
+solucion= dsolve(ed,f(x), ics=ci)
+print("El resultado es: ")
+pprint(solucion)
