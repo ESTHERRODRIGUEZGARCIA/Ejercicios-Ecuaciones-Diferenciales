ffda2255a7f3f51e6c7de855d417d7270c32b683
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-12 19:12:33 +0200

    poo

--- ejercicios/ej1/ejer1.py
+++ ejercicios/ej1/ejer1.py
@@ -6,12 +6,16 @@ import sympy as sp
 import math
 from sympy import init_printing
 
-x,y = symbols("x y")
-f,g =map(Function,'fg')
+class Primero:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        x,y = symbols("x y")
 
-#ED : y'=y*x*sin(x)
-ed = Eq(f(x)*Derivative(f(x),x),(f(x)+1)**2/(f(x)**2)
-eq = y(x)*ln(x)*y(x).diff(x)-((y(x)+1)/(x))**2
-sol = dsolve(ed,f(x))
-print("El resultado es:")
-pprint(sol)
+    def ecuacion1(self, x, y):
+        #ED : y'=(x**2*y-y)/(y+1)
+        ed = Eq(Derivative(y,x),((y*(x**2)-y)/(y+1))
+        
+        sol = dsolve(ed,y)
+        print("El resultado es:")
+        pprint(sol)
