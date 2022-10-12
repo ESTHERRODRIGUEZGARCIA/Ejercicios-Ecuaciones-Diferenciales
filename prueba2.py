c0e5fe85b7b7f8064a20e64e7cc2cff952a2a280
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-12 20:36:42 +0200

    solo no me funciona el 1 y es que no sé porqué

--- prueba2.py
+++ prueba2.py
@@ -1,3 +1,4 @@
+#importando funciones 
 from sympy import *
 import sympy
 import math
@@ -7,28 +8,37 @@ import scipy as sp
 from scipy.integrate import odeint
 from scipy.integrate import solve_ivp
 import numpy as np
-
-
-x,y = symbols('x,y')
-f,g = map(Function, 'fg')
-
-#Ecuacion 1
-print("Ecuacion 1:")
-
-ed1 = Eq(Derivative(f(x),x),(f(x)(x*2)-f(x))/(f(x)+1))
-pprint(ed1)
-ci = {f(3): -1}
-
-solucion1= dsolve(ed1,f(x), ics=ci)
-print("El resultado es: ")
-pprint(solucion1)
+import matplotlib.pyplot as plt
 
 x,y = symbols('x,y')
 f,g = map(Function, 'fg')
-print("Ecuación 1: ")
-ed1= Eq(Derivative(f(x),x),(f(x)(x**2)-f(x))/(f(x)+1))
-pprint(ed1)
-ci = {f(3): -1}
-solution1= dsolve(ed1,f(x), ics=ci)
+
+
+
+#Ecuacion 2
+print("Ecuacion 2:")
+ed2= Eq(Derivative(f(x),x)*sin(x),f(x)*log(f(x)))
+pprint(ed2)
+ci = {f(math.pi): math.e}
+
+solucion2= dsolve(ed2,f(x), ics=ci)
 print("El resultado es: ")
-pprint(solution1)
+pprint(solucion2)
+
+#Ecuacion 3
+print("Ecuacion 3:")
+ed3= Eq(Derivative(f(x),x)-(f(x)/(x-2)),2*(x-2)**2)
+pprint(ed3)
+
+solucion3= dsolve(ed3,f(x))
+print("El resultado es: ")
+pprint(solucion3)
+
+#Ecuacion 4
+print("Ecuacion 3:")
+ed4= Eq(Derivative(f(x),x)*2*x-f(x),3*x**2)
+pprint(ed4)
+
+solucion4= dsolve(ed4,f(x))
+print("El resultado es: ")
+pprint(solucion4)
