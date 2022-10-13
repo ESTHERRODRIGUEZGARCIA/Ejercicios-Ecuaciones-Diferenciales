ade66acaa83613a49d119199132edab86baab4c4
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-13 20:10:10 +0200

    no va

--- grafica.py
+++ grafica.py
@@ -1,9 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sympy import *
-import math
 import numpy as np
-from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 
@@ -14,12 +12,9 @@ x0 = 0 # initial amount
 
 ed4= Eq(Derivative(f(x),x)*2*x-(f(x)),3*x**2)
 index_set = f(x)
-x = np.zeros(ed4)
-
-x[0] = x0
 
 
-plt.plot(ed4, x)
+plt.plot(ed4, f(x))
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
