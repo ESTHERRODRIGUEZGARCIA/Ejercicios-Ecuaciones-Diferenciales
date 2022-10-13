cc0654e84fb69831e156851712e5a714dad034a6
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-13 20:26:28 +0200

    ec 4

--- ejercicios/ecuaciones.py
+++ ejercicios/ecuaciones.py
@@ -43,3 +43,18 @@ class EDO():
         solucion3= dsolve(ed3,f(x))
         print("El resultado es: ")
         pprint(solucion3)
+
+    def ecuacion4():
+        x,y = symbols('x,y')
+        f,g = map(Function, 'fg')
+
+        #Ecuacion 4
+        print("Ecuacion 4:")
+        ed4= Eq(Derivative(f(x),x)*2*x-f(x),3*x**2)
+        pprint(ed4)
+
+        solucion4= dsolve(ed4,f(x))
+        print("El resultado es: ")
+        pprint(solucion4)
+
+        
