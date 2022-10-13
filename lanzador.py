1307456e8d35456835a9672cdaae27dbfb206f6e
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-13 20:34:36 +0200

    () era el fallo

--- lanzador.py
+++ lanzador.py
@@ -1,10 +1,10 @@
 from ejercicios.ecuaciones import Ejers
 def ejecutar():
-    Ejers.ecuacion1
-    Ejers.ecuacion2
-    Ejers.ecuacion3
-    Ejers.ecuacion4
-    Ejers.ecuacion5
+    Ejers.ecuacion1()
+    Ejers.ecuacion2()
+    Ejers.ecuacion3()
+    Ejers.ecuacion4()
+    Ejers.ecuacion5()
 
 
 # def ejecutar(): para ver que ec hacer
